cc3705309588b0f23a946300935618bb62353082
Stefan Ganchev <56921662+SPGanchev18@users.noreply.github.com>
2020-11-02 20:19:38 +0000

    Update sets_mathematics.cpp

--- sets_mathematics/sets_mathematics.cpp
+++ sets_mathematics/sets_mathematics.cpp
@@ -1,8 +1,5 @@
-// sets_mathematics.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-#include "Header.h"
+#include <iostream> //declaring libraries
+#include <Header.h>
 using namespace std;
 
 //podmnojestva (chastichni i pulni)
@@ -17,49 +14,45 @@ using namespace std;
 
 
 
-int main()
+int main() //declaring the main function
 {
-	cout << "----------PLEASE ENETER VALUES BEFORE PROCEEDING OT THE PROGRAM---------- " << endl;
-	cout << endl;
+    cout << "----------PLEASE ENETER VALUES BEFORE PROCEEDING OT THE PROGRAM---------- " << endl;
+    cout << endl; //output the text in the quotes
 
-	float masivA[200], masivB[200];
-	int n = 0, m = 0;
+    float masivA[200], masivB[200]; //declaring arrays called masivA and masivB
+    int n = 0, m = 0; //declaring variables with value = 0
 
-	cout << "Enter how many elements you want to have in the first set (from 2 to 100): ";
+    cout << "Enter how many elements you want to have in the first set (from 2 to 100): "; //have to find out how to display error message if wrong data is entered in while cycles!!!
 
-	//have to find out how to display error message if wrong data is entered in while cycles!!!
+    do {
 
-	do {
+        cin >> n; //input a number from a variable n
 
-		cin >> n;
+    } while (n < 2 or n > 100); // while loop that counts all numbers from 2 to 100
 
-	} while (n < 2 or n > 100);
+    cout << "Enter the elements of the first array: " << endl; //output the text in the quotes
 
-	cout << "Enter the elements of the first array: " << endl;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> masivA[i];
-	}
+    for (int i = 0; i < n; i++) // Enter the elements of the array masivA
+    {
+        cin >> masivA[i]; 
+    }
 
 
-	cout << "Enter how many elements you want to have in the second set (from 2 to 100): ";
-	do {
+    cout << "Enter how many elements you want to have in the second set (from 2 to 100): "; //output the text in the quotes
+    do {
 
-		cin >> m;
+        cin >> m; //input variable m 
 
-	} while (m < 2 or m > 100);
+    } while (m < 2 or m > 100); //while loop
 
-	cout << "Enter the elements of the second array: " << endl;
+    cout << "Enter the elements of the second array: " << endl; //output the text in the quotes
 
-	for (int i = 0; i < m; i++)
-	{
-		cin >> masivB[i];
-	}
+    for (int i = 0; i < m; i++)  // Enter the elements of the array masivB
+    {
+        cin >> masivB[i];
+    }
 
-	bool doShowMenu = true;
-	do {
-		doShowMenu = mainMenu(masivA, masivB, n, m);
-	} while (doShowMenu);
-    
-}
+    bool doShowMenu = true; //set the varible doShowMenu to be true
+    do {
+        doShowMenu = mainMenu(masivA, masivB, n, m); //shows the numbers from the array
+    } while (doShowMenu); //while loop
